7ca40812140ed765ecd0ad108a9222b94d41363a
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-08-19 11:18:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Programm_black_list__denied_apps_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
